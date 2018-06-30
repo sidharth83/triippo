e8837635a1a638c612cd5d0c7380d6adaf964373
Sidharth Shanbhag <sidharth.mobile@gmail.com>
2018-06-30 22:51:56 +0100

    Change travelzeus to triippo

--- index.py
+++ index.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, url_for, request
-from travelzeus.dbtravel import getCities, getLocDetails, getCityAttractions
+from triippo.dbtravel import getCities, getLocDetails, getCityAttractions
 import pdb
 app = Flask(__name__)
 
